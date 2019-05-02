174b973c254d700330956f5e85aa156207e87542
Amanda <AmandaHenrique>
2019-05-02 11:44:24 -0300

    Mensagem de sucesso

--- app/views.py
+++ app/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from app.forms import PedidoForm
 
 def fazer_pedido(request):
+    msg = ''
     formulario = PedidoForm(request.POST or None)
     if formulario.is_valid():
         formulario.save()
         formulario = PedidoForm() # para apagar os inputs depois que apertei no submit
-
-    msg = "Pedido realizado com sucesso"
+        msg = "Pedido realizado com sucesso"
+    
     contexto = {
         'form' : formulario,
         'msg' : msg 
